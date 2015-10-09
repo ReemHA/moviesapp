8a2104699cca279ba68c93df0842df4bdb07a3b2
ReemHA <ReemHA@users.noreply.github.com>
2015-10-09 13:51:14 +0200

    reviews and trailers are displayed

--- app/src/main/java/com/example/user/movieproject/sync/MovieSyncAdapter.java
+++ app/src/main/java/com/example/user/movieproject/sync/MovieSyncAdapter.java
@@ -218,7 +218,6 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         if (cVV.size() > 0) {
             ContentValues[] cvArray = new ContentValues[cVV.size()];
             cVV.toArray(cvArray);
-            getContext().getContentResolver().delete(MovieContract.TrailersEntry.CONTENT_URI, null, null);
             getContext().getContentResolver().bulkInsert(MovieContract.TrailersEntry.CONTENT_URI, cvArray);
         }
         return;
@@ -242,7 +241,6 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         if (cVV.size() > 0) {
             ContentValues[] cvArray = new ContentValues[cVV.size()];
             cVV.toArray(cvArray);
-            getContext().getContentResolver().delete(MovieContract.ReviewsEntry.CONTENT_URI, null, null);
             getContext().getContentResolver().bulkInsert(MovieContract.ReviewsEntry.CONTENT_URI, cvArray);
             Log.d(LOG_TAG, "Sync Complete. " + cVV.size() + " Inserted");
         }
